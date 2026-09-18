807ae31f493ace47c5b38e675492b37fb6ef4ca5
vinaybelagali9-cmyk <vinaybelagali9@gmail.com>
2026-09-18 10:02:57 +0530

    Update age.py

--- age.py
+++ age.py
@@ -1,5 +1,6 @@
-age = ("enter your age")
-if(age>=18):
-print("you are eligible to vote")
+age = int(input("Enter your age: "))
+
+if age >= 18:
+  print("You are eligible to vote.")
 else:
-print("you are not eligible to vote")
+  print("You are not eligible to vote.")
